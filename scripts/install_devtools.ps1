f7e2f27cf6a3e57dc756bc5c50c9754365979afc
Anav-bain <anav.sobti@bain.com>
2026-09-23 19:22:03 +0530

    feat: add python launcher, python v3.10-3.14, volta install npm and coding agents

--- scripts/install_devtools.ps1
+++ scripts/install_devtools.ps1
@@ -1,3 +1,15 @@
-winget install python.python.3.9 python.python.3.10 python.python.3.11 python.python.3.12 python.python.3.13 astral-sh.uv docker.dockerdesktop docker.dockercompose docker.dockercli microsoft.visualstudiocode volta.volta git.git github.gitlfs
+winget install python.launcher astral-sh.uv docker.dockerdesktop docker.dockercompose docker.dockercli microsoft.visualstudiocode volta.volta git.git github.gitlfs
+
+winget install Python.Python.3.10
+winget install Python.Python.3.11
+winget install Python.Python.3.12
+winget install Python.Python.3.13
+winget install Python.Python.3.14
 
 winget install github.gitlfs rustlang.rust.gnu rustlang.rust.msvc rustlang.rustup golang.go git-cola.git-cola uutils.coreutils msys2.msys2 spicetify.spicetify eclipseadoptium.temurin.21.jdk burntsushi.ripgrep.msvc jesseduffield.lazygit postman.postman microsoft.azurecli gnuwin32.findutils gnuwin32.grep gnuwin32.tree gnuwin32.which llvm.llvm doist.todoist bitwarden.bitwarden bruno.bruno notion.notion brave.brave 9nt1r1c2hh7j
+
+volta install node@latest npm@22 yarn@latest
+
+npm i -g @openai/codex@latest
+npm i -g @anthropic-ai/claude-code@latest
+npm i -g opencode-ai
